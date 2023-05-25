1a3824dbde91bc40face451c7a55815ff651fe9f
MartinFlischman <mflischman@icloud.com>
2023-05-25 08:45:15 +0200

    Initial Commit

---
+++ War Card Game/War_Card_GameApp.swift
@@ -0,0 +1,17 @@
+//
+//  War_Card_GameApp.swift
+//  War Card Game
+//
+//  Created by Martin on 2023/05/25.
+//
+
+import SwiftUI
+
+@main
+struct War_Card_GameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
